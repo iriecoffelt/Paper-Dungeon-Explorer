e21f4e6f618678342fb84bd1b28221c8d1c9c502
Banana Munk <iriecoffelt@gmail.com>
2024-11-27 14:29:20 -0600

    State Machine
    - Adding Player state machine

---
+++ Scripts/General/GameConstants.cs
@@ -0,0 +1,21 @@
+/// <summary>
+/// Constants used in the game
+/// </summary>
+public class GameConstants
+{
+    // Animations
+    public const string ANIM_IDLE = "Idle";
+    public const string ANIM_MOVE = "Move";
+    public const string ANIM_DASH = "Dash";
+
+    // Inputs
+    public const string INPUT_MOVE_FORWARD = "MoveForward";
+    public const string INPUT_MOVE_BACKWARD = "MoveBackward";
+    public const string INPUT_MOVE_LEFT = "MoveLeft";
+    public const string INPUT_MOVE_RIGHT = "MoveRight";
+    public const string INPUT_DASH = "Dash";
+
+    // Notification
+    public const int NOTIFICATION_ENTER_STATE = 10241995;
+    public const int NOTIFICATION_EXIT_STATE = 19951024;
+}
