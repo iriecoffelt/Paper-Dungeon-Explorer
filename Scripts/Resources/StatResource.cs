9b763476ed62a0b586fc6ad9f0a399e38b5c46ea
Banana Munk <iriecoffelt@gmail.com>
2024-12-01 16:58:08 -0600

    even more things

--- Scripts/Resources/StatResource.cs
+++ Scripts/Resources/StatResource.cs
@@ -4,7 +4,8 @@ using Godot;
 [GlobalClass] //tells godot to treat this as a global resource
 public partial class StatResource : Resource
 {
-    public Action OnZero;
+    public event Action OnZero;
+    public event Action OnUpdate;
     [Export] public Stat StatType { get; private set; }
 
     private float _statValue;
@@ -14,6 +15,7 @@ public partial class StatResource : Resource
         set 
         {
             _statValue = Mathf.Clamp( value, 0, Mathf.Inf); //clamps the value between 0 and infinity
+            OnUpdate?.Invoke();
             if (_statValue == 0)
             {
                 OnZero?.Invoke();
--- Scripts/UI/UIController.cs
+++ Scripts/UI/UIController.cs
@@ -20,5 +20,7 @@ public partial class UIController : Control
     {
         GetTree().Paused = false;
         containers[ContainerType.Start].Visible = false;
+        containers[ContainerType.Stats].Visible = true;
+        GameEvents.RaiseStartGame();
     }
 }
